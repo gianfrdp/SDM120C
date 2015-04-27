f47e48bfb4e19083357cfaaa0c8d843d13867572
Gianfranco Di Prinzio <gianfrdp@inwind.it>
2015-04-27 10:48:13 +0200

    Added live production for pool123-1.5

--- metern/comapps/pool123-1.5.php
+++ metern/comapps/pool123-1.5.php
@@ -1,15 +1,20 @@
 <?php
-if ($_SERVER['SERVER_ADDR'] != '127.0.0.1') {
+if ($_SERVER['SERVER_ADDR'] != '127.0.0.1' && $_SERVER['SERVER_ADDR'] != 'localhost') {
     die('Direct access not permitted');
 }
+
 // This script will output a 123solar counter into a meterN compatible format
 // You'll need to setup the path to 123, your inverter number and your meter id
 // Request live command with "curl http://localhost/metern/comapps/pool123solar.php?cmd=1"
 // Main command with "curl http://localhost/metern/comapps/pool123solar.php?cmd=2"
 
 $pathto123s = '/srv/http/123solar';
+//$pathto123s = '/var/www/123solar';
+// for multiple inverters
 $invtnums   = array (0 => 1, 1 => 2);
 $meterid    = 4;
+//$server = 'http://192.168.2.12/123solar/programs/programmultilive.php';
+$server = 'http://localhost/123solar/programs/programmultilive.php';
 
 //parse_str(implode('&', array_slice($argv, 1)), $_GET);
 
@@ -22,6 +27,7 @@ if (!empty($_GET['cmd']) && is_numeric($_GET['cmd'])) {
 
 define('checkaccess', TRUE);
 include("$pathto123s/config/config_main.php");
+include("$pathto123s/scripts/memory.php");
 
 date_default_timezone_set($DTZ);
 
@@ -57,8 +63,17 @@ if ($cmd == 2 && (empty($shmid) || $KWHT == 0)) { // 123s ain't running at night
     }
 }
 
-if ($cmd == 1 && empty($shmid)) { // 123s ain't running
+if ($cmd == 1) {
     $GP = 0;
+
+    $data = file_get_contents($server);
+    $memarray = json_decode($data, true);
+    $GP = $memarray[0]['GPTOT'];
+    if ($GP > 1000) {
+        $GP = round($GP, 0);
+    } else {
+        $GP = round($GP, 1);
+    }
 }
 
 if ($cmd == 1) {
